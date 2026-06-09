32bcff74f6147f408609fb59589559002b67e5a5
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-06-09 15:46:53 +0600

    reverse string

---
+++ problem.js
@@ -0,0 +1,12 @@
+// Day 2 — Strings & String Methods
+// Problem 6: Reverse a String
+
+function reverseString(str) {
+    return str.split('').reverse().join('')
+}
+
+console.log(reverseString("hello"))
+
+
+// Problem 7: Count Vowels in a String 
+
